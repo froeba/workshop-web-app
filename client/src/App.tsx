a731c4bf49c8ec188d584cbdabb154ae6e5106e8
Bert Froeba <bert.froeba@arm.com>
2019-11-07 17:41:41 +0000

    add more info

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,34 @@
+import moment from "moment";
 import React, { useEffect, useState } from "react";
-import superagent from "superagent";
 import {
-  LineChart,
   Line,
-  YAxis,
+  LineChart,
   ResponsiveContainer,
   Tooltip,
-  XAxis
+  XAxis,
+  YAxis
 } from "recharts";
+import superagent from "superagent";
 import "./App.css";
-import moment from "moment";
+import { resolvePlugin } from "@babel/core";
+import { access } from "fs";
+
+const deviceNames: { [index: string]: string } = {
+  "016981016ceb00000000000100100100": "Disco 1",
+  "016b9e62feda000000000001001000fa": "K64F 1",
+  "016badc3e46400000000000100100312": "K64F 2",
+  "016bade3650a000000000001001000d0": "Odin 1",
+  "016bade3be1e000000000001001000d3": "Odin 2",
+  "016e12b596320000000000010010fe40": "K64F 3",
+  "016e12ed701500000000000100110272": "K64F 4",
+  "016e12f203360000000000010011032f": "K64F 5"
+};
+
+const resourceNames: { [index: string]: string } = {
+  "/3303/.*/5700": "Temperature",
+  "/3304/.*/5700": "Relative Humidity",
+  "/3323/.*/5700": "Air pressure"
+};
 
 interface ResourceValue {
   id: number;
@@ -62,14 +81,35 @@ const App: React.FC = () => {
   const showDevice = (paths: Paths, deviceId: string) =>
     Object.keys(paths)
       .sort((a, b) => a.localeCompare(b))
-      .map(res => (
-        <div className="App-graph" key={res}>
-          <h3 title={deviceId}>
-            {`${deviceId.slice(0, 6)}...${deviceId.slice(-6)}`}- {res}
-          </h3>
-          {showPath(paths[res])}
-        </div>
-      ));
+      .map(res => {
+        const deviceName = deviceNames[deviceId]
+          ? deviceNames[deviceId]
+          : `${deviceId.slice(0, 6)}...${deviceId.slice(-6)}`;
+        const matchPath = Object.keys(resourceNames)
+          .map(e => (res.match(e) ? e : false))
+          .reduce((acc, cur) => (!!cur ? cur : acc), "");
+        const resourceName =
+          matchPath && resourceNames[matchPath]
+            ? resourceNames[matchPath]
+            : res;
+        return (
+          <div className="device" key={res}>
+            <h3 title={deviceId}>
+              {deviceName} - {resourceName}
+            </h3>
+            <div className="App-graph">
+              <div className="graph">{showPath(paths[res])}</div>
+              <div className="value">
+                <h1>
+                  Value
+                  <br />
+                  {paths[res][paths[res].length - 1].value.toFixed(1)}
+                </h1>
+              </div>
+            </div>
+          </div>
+        );
+      });
 
   const showPath = (values: ResourceValue[]) => {
     const max = Math.ceil(
@@ -86,7 +126,12 @@ const App: React.FC = () => {
     return (
       <ResponsiveContainer aspect={21 / 9} minHeight={200}>
         <LineChart data={values}>
-          <Line dot={false} type="monotone" dataKey="value" />
+          <Line
+            dot={false}
+            type="monotone"
+            dataKey="value"
+            animationEasing="linear"
+          />
           <XAxis
             scale="time"
             dataKey="epoch"

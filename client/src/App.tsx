9852bae35b00b482ab276c667bedcd5492ca563c
Bert Froeba <bert.froeba@arm.com>
2019-11-26 16:10:33 +0000

    whiteForNoChange

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -67,7 +67,8 @@ const App: React.FC = () => {
           .reduce((acc, cur) => (!!cur ? cur : acc), "");
         const resourceName = matchPath && resourceNames[matchPath] ? resourceNames[matchPath] : res;
         const [val1, val2] = paths[res];
-        const styleColour = val1 && val2 ? (val1.value > val2.value ? "green" : "red") : "white";
+        const styleColour =
+          val1 && val2 && val1.value !== val2.value ? (val1.value > val2.value ? "green" : "red") : "white";
         return (
           <div className="device" key={res}>
             <h3 title={deviceId}>
